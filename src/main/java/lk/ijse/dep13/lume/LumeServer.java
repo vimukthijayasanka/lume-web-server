ccaa7c5351a4273edd17927e6450e19946d93caf
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-02-04 20:25:13 +0530

    add httpRequest reader

--- src/main/java/lk/ijse/dep13/lume/LumeServer.java
+++ src/main/java/lk/ijse/dep13/lume/LumeServer.java
@@ -1,16 +1,21 @@
 package lk.ijse.dep13.lume;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class LumeServer {
+    private static Socket localSocket;
+
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(80);
             System.out.println("Lume Server waiting for connection...");
             while (true) {
-                Socket localSocket = serverSocket.accept();
+                localSocket = serverSocket.accept();
                 System.out.println("connection accepted from " + localSocket.getRemoteSocketAddress());
 
                 new Thread(() -> {
@@ -25,4 +30,23 @@ public class LumeServer {
             throw new RuntimeException(e);
         }
     }
+
+    private void readHttpRequest(){
+        try {
+            InputStream is = localSocket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader reader = new BufferedReader(isr);
+
+            String cmdLine = reader.readLine();
+            String[] array = cmdLine.split(" ");
+            String command = array[0];
+            String resourcePath = array[1];
+            System.out.println(command + " " + resourcePath);
+            
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
