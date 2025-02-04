cbd2582d41c227bda4b8b41cc2846be379e0e361
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-02-04 15:49:52 +0530

    create server socket

--- src/main/java/lk/ijse/dep13/lume/LumeServer.java
+++ src/main/java/lk/ijse/dep13/lume/LumeServer.java
@@ -1,4 +1,20 @@
 package lk.ijse.dep13.lume;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class LumeServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(80);
+            System.out.println("Lume Server waiting for connection...");
+            while (true) {
+                Socket localSocket = serverSocket.accept();
+                System.out.println("connection accepted from " + localSocket.getRemoteSocketAddress());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
